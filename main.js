9af4d7e7a6c3cfc9bdefbdcc06c81b14f08d1a71
lucasquetinho <105065904+lucasquetinho@users.noreply.github.com>
2024-04-15 21:35:07 -0300

    terminei o curso

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+//------------------------Defini-as-váriaveis--------------------------------------//
+
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
-
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca')
+
+//------------------------Defini-a-função-dos-botões/checbox--------------------------------------//
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -24,18 +34,11 @@ function aumentaTamanho(){
     geraSenha();
 }
 
-const campoSenha = document.querySelector('#campo-senha');
-
-const checkbox = document.querySelectorAll('.checkbox');
-
 for (i=0; i < checkbox.length;i++){
     checkbox[i].onclick = geraSenha;
 }
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
-const numeros = '0123456789';
-const simbolos = '!@%*?';
+//------------------------Gera-a-senha---------------------------------//
 
 geraSenha();
 
@@ -54,7 +57,6 @@ function geraSenha(){
     if (checkbox[3].checked){
         alfabeto = alfabeto + simbolos;
     }
-    console.log(alfabeto);
 
     let senha = '';
     for (let i = 0; i < tamanhoSenha;i++){
@@ -63,4 +65,22 @@ function geraSenha(){
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
 }
+
+//------------------------Defini-o-nível-de-força-da-senha---------------------------------//
+
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto)
+
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (entropia > 57){
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57 ) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35){
+        forcaSenha.classList.add('fraca');
+    }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para achar a senha";
+}   
